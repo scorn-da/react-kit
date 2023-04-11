26d372c71145ff1d476d4999ed5bd9c6c47bca37
Дмитрий Варганов <varganovda@gmail.com>
2023-04-11 20:42:04 +0300

    feat(ui): added a posts search

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import "src/styles/App.css";
 import Posts from "src/components/Posts/Posts";
 import PostForm from "src/components/PostForm/PostForm";
@@ -12,6 +12,18 @@ function App() {
     {id: 3, title: 'Python', text: 'Python — ЯП',},
   ]);
   const [selectedSort, setSelectedSort] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const sortedPosts = useMemo(() => {
+    if(selectedSort) {
+      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+    }
+    return posts;
+  }, [selectedSort, posts]);
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()));
+  }, [searchQuery, sortedPosts]);
 
   function createPost(newPost) {
     setPosts([...posts, newPost]);
@@ -23,7 +35,6 @@ function App() {
 
   function sortPosts(sort) {
     setSelectedSort(sort);
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
   }
 
   return (
@@ -31,7 +42,11 @@ function App() {
       <PostForm create={createPost} />
       <hr style={{margin: '12px 0'}} />
       <div>
-        <Input placeholder="Поиск..." />
+        <Input
+            value={searchQuery}
+            onChange={evt => setSearchQuery(evt.target.value)}
+            placeholder="Поиск..."
+        />
         <Select
             value={selectedSort}
             onChange={sortPosts}
@@ -49,8 +64,8 @@ function App() {
         />
       </div>
       {
-        posts.length > 0
-            ? <Posts remove={removePost} title="Список постов" posts={posts} />
+        sortedAndSearchedPosts.length > 0
+            ? <Posts remove={removePost} title="Список постов" posts={sortedAndSearchedPosts} />
             : <h1 style={{textAlign: 'center'}}>Постов пока нет</h1>
       }
     </div>
