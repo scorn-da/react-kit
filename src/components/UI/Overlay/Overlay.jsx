cb348ebba7fb5ad7dcc1b9888519f125bf61d24f
Дмитрий Варганов <varganovda@gmail.com>
2023-04-12 12:51:16 +0300

    fix(ui): fixed the styles import in the overlay component

--- src/components/UI/Overlay/Overlay.jsx
+++ src/components/UI/Overlay/Overlay.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import cn from 'classnames';
-import styles from 'Overlay.module.css';
+import styles from './Overlay.module.css';
 
 const Overlay = ({children, isActive}) => {
   return (
