b63fd53fd327b95b43b1005717c3f6e5f44de4e2
Дмитрий Варганов <varganovda@gmail.com>
2023-04-13 17:40:42 +0300

    feat(api): added a service to fetch posts

---
+++ src/api/PostService.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default class PostService {
+  static async getAll() {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    return res.data;
+  }
+}
