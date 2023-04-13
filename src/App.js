029437f3a2466d3eeff8461b1700518def104198
Дмитрий Варганов <varganovda@gmail.com>
2023-04-13 17:43:24 +0300

    feat(ui): added posts fetching and a loader to wait the process

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "src/styles/App.css";
 import Posts from "src/components/Posts/Posts";
 import PostForm from "src/components/PostForm/PostForm";
@@ -6,19 +6,29 @@ import PostsFilter from "src/components/PostsFilter/PostsFilter";
 import ModalWithOverlay from "src/components/UI/Modal/ModalWithOverlay/ModalWithOverlay";
 import Button from "src/components/UI/Button/Button";
 import { usePosts } from "src/hooks/usePosts";
+import PostService from "src/api/PostService";
+import Loader from "src/components/UI/Loader/Loader";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'JavaScript', body: 'JavaScript — ЯП',},
-    {id: 2, title: 'Java', body: 'Java — ЯП',},
-    {id: 3, title: 'Python', body: 'Python — ЯП',},
-  ]);
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({
     sort: '',
     query: '',
   });
   const [isModalActive, setIsModalActive] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+    setIsLoading(true);
+    const posts = await PostService.getAll();
+    setPosts(posts);
+    setIsLoading(false);
+  }
 
   function createPost(newPost) {
     setPosts([...posts, newPost]);
@@ -33,7 +43,11 @@ function App() {
     <div className="App">
       <hr style={{margin: '12px 0'}} />
       <PostsFilter filter={filter} setFilter={setFilter} />
-      <Posts remove={removePost} title="Список постов" posts={sortedAndSearchedPosts} />
+      {
+        isLoading
+            ? <Loader />
+            : <Posts remove={removePost} title="Список постов" posts={sortedAndSearchedPosts} />
+      }
       <Button style={{margin: '12px 0'}} onClick={() => setIsModalActive(true)}>Создать пост</Button>
       <ModalWithOverlay isActive={isModalActive} setIsActive={setIsModalActive}>
         <PostForm create={createPost} />
