f00eae0b0d61d1c4b029fe0caf82bdf5dd7d2822
Дмитрий Варганов <varganovda@gmail.com>
2023-05-08 16:42:07 +0300

    feat(ui): added links to post pages

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import styles from './Post.module.css'
 import Button from "src/components/UI/Button/Button";
+import { NavLink, Route } from "react-router-dom";
 
 const Post = (props) => {
-
   return (
       <div key={props.post.id} className={styles.post}>
-        <div>
+        <div className={styles.content}>
           <b>{props.post.id} {props.post.title}</b>
           <p>{props.post.body}</p>
         </div>
-        <div>
+        <div className={styles.buttons}>
+          <NavLink to={`/posts/${props.post.id}`}>
+            <Button>Открыть</Button>
+          </NavLink>
           <Button onClick={() => props.remove(props.post)}>Удалить</Button>
         </div>
       </div>
